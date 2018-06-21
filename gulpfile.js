94c0d0274741a02f77e97a96a468d4792059d917
william-bratches <william.bratches@gmail.com>
2018-06-21 09:22:07 -0400

    added babel transpile

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,17 @@
 const gulp = require('gulp');
 const mocha = require('gulp-spawn-mocha');
+const babel = require('gulp-babel');
 
 const paths = {
+  src: ['src/**/*.js'],
   scripts: ['src/**/*.js', 'test/**/*.js'],
   test: ['test/**/*.js'],
 };
+
+gulp.task('build', () => gulp
+  .src(paths.src)
+  .pipe(babel({ presets: ['es2015'] }))
+  .pipe(gulp.dest('dist')));
 
 gulp.task('test', () => gulp
   .src(paths.test)
